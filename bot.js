7d88bbb4f76bc227409b7d4a0954ab99aa8b591a
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2018-11-17 01:16:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('ready', function(){
     var ms = 100000 ;
     var setGame = [`+help `,`PoWeR MC`];
