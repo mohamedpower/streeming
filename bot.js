5256b3596e43c187f83c61e434560cc581d9bd9a
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2018-11-17 00:55:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,28 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "+";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Death Shop`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
+client.on('ready', function(){
+    var ms = 100000 ;
+    var setGame = [`+help `,`PoWeR MC`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/falsteni_`);
+    }, ms);100000
+name: '-Universe Town` 1K-Soon........ ',
+    application_id: '353901189357764609',//هنا ايدي البوت 
+     assets: {
+         large_image:   `509420418453471236`, //هنا id صوره يلي جبت
+  
+    }
+  }
+    });
+  
+   
 });
 
 client.login(process.env.BOT_TOKEN);
